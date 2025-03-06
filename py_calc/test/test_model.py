48e88641caa6eb311b2a97ed640fc811e7882acc
ConnorDonegan <connor.donegan@gmail.com>
2025-03-06 15:14:07 -0600

    empty contents

---
+++ py_calc/test/test_model.py
@@ -0,0 +1,8 @@
+# def test_evaluateExpression():
+#    """ 
+#    Test of evaluateExpression
+#    tests that the expression is returned as a string
+#    Tests that mathematical expressions are evaluated before conversion to string
+#    """
+#    assert isinstance(evaluateExpression("4.0"), str)
+#    assert evaluateExpression("4.0 + 1") == "5.0"
