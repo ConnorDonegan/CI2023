92b96d8364a96bc98a8213fcbf6482c8667442b4
ConnorDonegan <connor.donegan@gmail.com>
2025-03-06 15:01:49 -0600

    rm the test

--- py_calc/test/test_controller.py
+++ py_calc/test/test_controller.py
@@ -1,3 +1,8 @@
+def test_calculateResult(controller):
+    controller._view.setDisplayText("3+3")
+    controller._calculateResult()
+    assert controller._view.displayText() == "6"
+
 def test_returnSignal(controller):
     """Tests the Return key binding interface to our Qt display widget."""
     from PyQt5 import QtCore, QtGui
@@ -7,4 +12,6 @@ def test_returnSignal(controller):
         QtCore.QEvent.KeyPress, QtCore.Qt.Key_Enter, QtCore.Qt.NoModifier
     )
     controller._view.display.keyPressEvent(event)
+    controller._calculateResult()
+    print("The result is:", controller._view.displayText())
     assert controller._view.displayText() == "3"
